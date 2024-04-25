922d8c9b1f869eb1ba19d507bbeef90bd2a5f45b
sadwikandula2 <123432491+sadwikandula2@users.noreply.github.com>
2024-04-24 22:35:56 -0700

    Linked-List-1 Done

--- Sample.java
+++ Sample.java
@@ -1,24 +1,86 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Problem1 (https://leetcode.com/problems/reverse-linked-list/)
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode curr=head;
+        ListNode prev=null;
+        while(curr!=null)
+        {
+            ListNode temp=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        return prev;
+    }
+}
 
 
-// Your code here along with comments explaining your approach
 
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Problem2 (https://leetcode.com/problems/remove-nth-node-from-end-of-list/)
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next=head;
+        ListNode slow=dummy, fast=dummy;
+        for(int i=0;i<=n;i++)
+        {
+            fast=fast.next;
+        }
+        while (fast!=null)
+        {
+            fast=fast.next;
+            slow=slow.next;
+        }
+        slow.next=slow.next.next;
+        return dummy.next;   
+    }
+}
 
 
-// Your code here along with comments explaining your approach
+
+// Problem3 (https://leetcode.com/problems/linked-list-cycle-ii/)
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        boolean flag=false;
+        while(fast!=null && fast.next!=null)
+        {
+            fast=fast.next.next;
+            slow=slow.next;
+            if(slow==fast)
+            {
+                flag=true;
+                break;
+            } 
+        }
+        if(flag)
+        {
+            slow=head;
+            while(slow!=fast)
+            {
+                slow=slow.next;
+                fast=fast.next;
+            }
+            return slow;
+        }
+        return null;
+    }
+}
 
 
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
 
 
-// Your code here along with comments explaining your approach
+
